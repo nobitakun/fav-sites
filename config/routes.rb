c2fe9a4e59ca98e4a01502b1a7a3fb39891f919b
Kuroiwa Taku <kuroiwa.taku@gmail.com>
2019-03-12 14:18:40 +0000

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  
+  resources :users
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# ユーザー登録
+root_user = User.new(name: 'root user', email: 'root@root.com', root: true, admin: true)
+root_user.password = 'root'
+root_user.password_confirmation = 'root'
+root_user.save!
+
+admin_user = User.new(name: '管理者', email: 'admin@admin.com', root: false, admin: true)
+admin_user.password = 'admin'
+admin_user.password_confirmation = 'admin'
+admin_user.save!
+
+test_user = User.new(name: 'テストユーザー', email: 'test@test.com', root: false, admin: false)
+test_user.password = 'test'
+test_user.password_confirmation = 'test'
+test_user.save!
