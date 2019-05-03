b15a542519aff58b4542f13e6ec40e629af6c3ce
Kuroiwa Taku <kuroiwa.taku@gmail.com>
2019-05-03 13:05:32 +0000

    change config

--- config/initializers/mysqlpls.rb
+++ config/initializers/mysqlpls.rb
@@ -1,11 +1,9 @@
 require 'active_record/connection_adapters/abstract_mysql_adapter'
 
-if Rails.env.development?
-  module ActiveRecord
-    module ConnectionAdapters
-      class AbstractMysqlAdapter
-        NATIVE_DATABASE_TYPES[:string] = { :name => "varchar", :limit => 191 }
-      end
+module ActiveRecord
+  module ConnectionAdapters
+    class AbstractMysqlAdapter
+      NATIVE_DATABASE_TYPES[:string] = { :name => "varchar", :limit => 191 }
     end
   end
 end
