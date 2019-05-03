95a9e50050801c0573a0190e5d48e2e5d26676a9
Kuroiwa Taku <kuroiwa.taku@gmail.com>
2019-05-03 13:45:56 +0000

    add production smtp setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { protocol: 'https' , host: ENV['HOST'] }
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    address: ENV['SMTP_ADDRESS'],
+    port: ENV['SMTP_PORT'],
+    domain: ENV['SMTP_DOMAIN'],
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASS'],
+    authentication: 'login',
+  }
+  
 end
